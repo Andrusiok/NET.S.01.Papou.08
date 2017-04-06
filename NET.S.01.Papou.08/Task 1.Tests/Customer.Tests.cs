ef3c15e110340ec0f206a05ea3d47d52408cbd3e
Andrusiok <angrey1996@yandex.ru>
2017-04-06 13:47:26 +0300

    Added Task1 with tests

---
+++ NET.S.01.Papou.08/Task 1.Tests/Customer.Tests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using System.Globalization;
+using Task_1;
+
+namespace Task_1.Tests
+{
+    public class Provider : IFormatProvider, ICustomFormatter
+    {
+        private IFormatProvider parent;
+
+        public Provider() : this(CultureInfo.CurrentCulture) { }
+
+        public Provider(IFormatProvider provider)
+        {
+            parent = provider;
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter)) return this;
+            return null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider provider)
+        {
+            if (ReferenceEquals(arg, null) || format != "P")
+                return string.Format(parent, "{0:" + format + "}", arg);
+
+            if (arg is Customer)
+            {
+                Customer customer = (Customer)arg;
+                StringBuilder result = new StringBuilder();
+                result.Append($"Name: {customer.Name}");
+                return result.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        public override string ToString() => "P";
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase]
+        public void Formatter_Positive()
+        {
+            Customer customer = new Customer("Andrej", "+375 (29) 181-16-41", 15.005m);
+
+            string result = string.Format(new Customer.DefaultProvider(), "{0:DF}", customer);
+
+            result = customer.ToString();
+            result = string.Format(new Provider(), "{0:P}", customer);
+            result = customer.ToString(new Provider());
+        }
+    }
+}
---
+++ NET.S.01.Papou.08/Task 1/Customer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task_1
+{
+    public class Customer
+    {
+        #region default format provider
+        public class DefaultProvider : IFormatProvider, ICustomFormatter
+        {
+            private IFormatProvider parent;
+
+            public DefaultProvider() : this(CultureInfo.CurrentCulture) { }
+
+            public DefaultProvider(IFormatProvider provider)
+            {
+                parent = provider;
+            }
+
+            public object GetFormat(Type formatType)
+            {
+                if (formatType == typeof(ICustomFormatter)) return this;
+                return null;
+            }
+
+            public string Format(string format, object arg, IFormatProvider provider)
+            {
+                if (ReferenceEquals(arg, null) || format != "DF")
+                    return string.Format(parent, "{0:" + format + "}", arg);
+
+                if (arg is Customer)
+                {
+                    Customer customer = (Customer)arg;
+                    StringBuilder result = new StringBuilder();
+                    result.Append($"Name: {customer.Name}, Contact Phone: {customer.ContactPhone}, Revenue: {customer.Revenue}");
+                    return result.ToString();
+                }
+
+                return string.Empty;
+            }
+
+            public override string ToString() => "DF";
+        }
+        #endregion
+
+        #region private fields
+        private string _name;
+        private string _contactPhone;
+        private decimal _revenue;
+        #endregion
+
+        #region public properties
+        public string Name => _name;
+        public string ContactPhone => _contactPhone;
+        public decimal Revenue => _revenue;
+        #endregion
+
+        #region public methods
+        public Customer(string name, string contactPhone, decimal revenue)
+        {
+            _name = name;
+            _contactPhone = contactPhone;
+            _revenue = revenue;
+        }
+
+        public override string ToString() => ToString(new DefaultProvider());
+
+        public string ToString(IFormatProvider formatProvider)
+        {
+            string result = string.Empty;
+
+            if (formatProvider is ICustomFormatter)
+            {
+                ICustomFormatter formatter = (ICustomFormatter)formatProvider;
+                result = formatter.Format(formatProvider.ToString(), this, formatProvider);
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
